4e57da4434dbbd3a305c538b1a698ccbaec2c1d7
Carlos Colmenares <carlosshmx@hotmail.com>
2022-05-28 04:31:15 -0300

    se logra hacer las actualizaciones entre base de datos y formularios =D

--- src/routes/lists.js
+++ src/routes/lists.js
@@ -40,48 +40,66 @@ router.post('/addlist', async(req, res) =>{
             res.redirect('/lists/addlist');
         }else{
             await pool.query('INSERT INTO dates set ?', [newDate]);
-            let id_date = await pool.query(`SELECT id FROM dates WHERE date = "${date}"`)
-            console.log(id_date[0].id);
+            let id_dateArray = await pool.query(`SELECT id FROM dates WHERE date = "${date}"`)
+            let id = id_dateArray[0].id
             const listElement = await pool.query('SELECT * FROM locales');
-            res.render('lists/editlist',{listElement, id_date, date} )
+
+            for(i=0; i<locales.length; i++){
+                    const newOperation = { 
+                        date: date,
+                        operations: null,
+                        local: locales[i],
+                        box: null, 
+                        qty_boxes: null, 
+                        id_date: id
+                    }; 
+            await pool.query('INSERT INTO operations set ?', [newOperation])
+            }
+
+
+            // res.render(`/lists/editlist`,{listElement, id, date} )
+            res.redirect(`/lists/editlist/${id}`);
         }
         
 });
 
 router.get('/editlist/:id', async(req, res) =>{
     const{id}= req.params;
-    const listElement = await pool.query('SELECT * FROM locales');
+    const locales = await pool.query('SELECT * FROM locales');
     const timestamp = await pool.query(`SELECT date FROM dates WHERE id = ${id}`);
+    const data = await pool.query(`SELECT * FROM operations INNER JOIN locales ON operations.local = locales.local WHERE id_date = ${id}`)
     let date = timestamp[0].date;
-    res.render('lists/editlist',{listElement, id, date} )
+    res.render('lists/editlist',{data, id, date} )
 });
 
 
 router.post('/editlist/:id', async (req, res) => {
     const {id} = req.params;
-    const {local, box, qty_boxes, operations} = req.body;
+    const {box, qty_boxes, operations} = req.body;
 
-    for(i=1; i<=13; i++){
+    for(i=0; i<locales.length; i++){
+        let local = locales[i]
         const newOperation = { 
-            box: box[i], 
-            qty_boxes: qty_boxes[i], 
-            operations: operations[i],
-            id_date: id,
-            local: locales[i-1]
-        };
-        console.log(newOperation);
-        await new Promise((resolve, reject) => {
-            resolve(pool.query('INSERT INTO operations set ?', [newOperation]))
-         
-        });
+            operations: operations[i]||null,
+            box: box[i]||null,
+            qty_boxes: qty_boxes[i]||null
+        }; 
+        await pool.query('UPDATE operations set ? WHERE id_date = ? AND local = ?', [newOperation, id, local]);   
     }
+    // console.log(newOperation);
 
-    
+    //     for(i=0; i<locales.length; i++){
+    //         const newOperation = { 
+    //             operations: operations[i],
+    //             box: box[i], 
+    //             qty_boxes: qty_boxes[i], 
+    //         }; 
+    // await pool.query('UPDATE operations set ? WHERE id_date ?', [newOperation, id])
+    // }
+
 
     // await pool.query('INSERT INTO operations set ?', [newOperation]);
     
-   
-    // await pool.query('UPDATE links set ? WHERE id = ?', [newLink, id]);
     // req.flash('success', 'Link updated successfuly');
     const listElement = await pool.query('SELECT * FROM dates')
     res.render('lists/list',{listElement})
